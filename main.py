3be7aecacf3b11eaf4efd1aa139114e3261a6c62
Anthony Delivanis <anthony.delivanis@gmail.com>
2019-10-06 12:48:31 -0700

    changed schema to ingest respective data types

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
+#!/usr/bin/env python3
+
+########
+# TO TEST LOCALLY
+# spark-submit --jars jar_files/hadoop-common-2.7.3.jar,jar_files/hadoop-aws-2.7.3.jar,jar_files/aws-java-sdk-1.7.4.jar main.py
+########
+
 import os
 import boto3
 from pyspark import SparkConf
 from pyspark.sql import SparkSession 
-from pyspark.sql.types import *
-
-########
-# TO TEST LOCALLY
-# pyspark --packages com.databricks:spark-redshift_2.10:3.0.0-preview1 --jars RedshiftJDBC42-no-awssdk-1.2.20.1043.jar,aws-java-sdk-1.11.534.jar
-# pyspark --packages com.databricks:spark-redshift_2.10:3.0.0-preview1,org.apache.hadoop:hadoop-aws:3.2.0 --jars RedshiftJDBC42-no-awssdk-1.2.20.1043.jar,jar_files
-# pyspark --packages com.databricks:spark-redshift_2.10:3.0.0-preview1,org.apache.hadoop:hadoop-aws:3.2.0 --jars jar_files
-# pyspark --packages org.apache.hadoop:hadoop-aws:3.2.0 --jars jar_files
-########
-# pyspark --packages org.apache.hadoop:hadoop-aws:2.7.0 --jars hadoop-aws-2.7.0.jar
-########
-# spark-submit --packages org.apache.hadoop:hadoop-aws:2.7.0 --jars hadoop-aws-2.7.0.jar --py-files dependencies.zip main.py
-# spark-submit --packages org.apache.hadoop:hadoop-aws:3.1.2
-# spark-submit --jars jar_files/hadoop-common-2.7.3.jar,jar_files/hadoop-aws-2.7.3.jar,jar_files/aws-java-sdk-1.7.4.jar main.py
+from pyspark.sql.types import StructType, StructField, DateType, StringType, TimestampType, IntegerType
+# from pyspark.sql.types import *
 
 aws_access_key = os.environ.get('AWS_ACCESS_KEY')
 aws_secret_key = os.environ.get('AWS_SECRET_KEY')
@@ -24,131 +19,103 @@ spark = SparkSession.builder \
                     .master('local') \
                     .appName('RedshiftEtl') \
                     .getOrCreate()
-                    # .config("fs.s3n.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem") \
-                    # .getOrCreate()
-# SparkConf().getAll()
 sc = spark.sparkContext
+sc._jsc.hadoopConfiguration().set('fs.s3a.access.key', aws_access_key)
+sc._jsc.hadoopConfiguration().set('fs.s3a.secret.key', aws_secret_key)
 
-sc._jsc.hadoopConfiguration().set("fs.s3a.access.key", aws_access_key)
-sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key", aws_secret_key)
-# sc._jsc.hadoopConfiguration().set("fs.s3n.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
-
-# Schema for the 18 columns of logs to be loaded as strings initially
+# Schema for the 18 columns of logs to be loaded per respective data types
 schema_18 = StructType([
-            StructField('date', StringType(), True),
-            StructField('time', StringType(), True), 
+            StructField('date', DateType(), True),
+            StructField('time', TimestampType(), True), 
             StructField('server_ip', StringType(), True),
             StructField('method', StringType(), True),
             StructField('uri_stem', StringType(), True),
             StructField('uri_query', StringType(), True),
-            StructField('server_port', StringType(), True),
+            StructField('server_port', IntegerType(), True),
             StructField('username', StringType(), True),
             StructField('client_ip', StringType(), True),
             StructField('client_browser', StringType(), True),
             StructField('client_cookie', StringType(), True),
             StructField('client_referrer', StringType(), True),
-            StructField('status', StringType(), True),
-            StructField('substatus', StringType(), True),
-            StructField('win32_status', StringType(), True),
-            StructField('bytes_sent', StringType(), True),
-            StructField('bytes_received', StringType(), True),
-            StructField('duration', StringType(), True)])
+            StructField('status', IntegerType(), True),
+            StructField('substatus', IntegerType(), True),
+            StructField('win32_status', IntegerType(), True),
+            StructField('bytes_sent', IntegerType(), True),
+            StructField('bytes_received', IntegerType(), True),
+            StructField('duration', IntegerType(), True)
+            ])
 
+# Schema for logs with 14 columns
 schema_14 = StructType([
-            StructField('date', StringType(), True),
-            StructField('time', StringType(), True), 
+            StructField('date', DateType(), True),
+            StructField('time', TimestampType(), True), 
             StructField('server_ip', StringType(), True),
             StructField('method', StringType(), True),
             StructField('uri_stem', StringType(), True),
             StructField('uri_query', StringType(), True),
-            StructField('server_port', StringType(), True),
+            StructField('server_port', IntegerType(), True),
             StructField('username', StringType(), True),
             StructField('client_ip', StringType(), True),
             StructField('client_browser', StringType(), True),
-            StructField('status', StringType(), True),
-            StructField('substatus', StringType(), True),
-            StructField('win32_status', StringType(), True),
-            StructField('duration', StringType(), True)])
+            StructField('status', IntegerType(), True),
+            StructField('substatus', IntegerType(), True),
+            StructField('win32_status', IntegerType(), True),
+            StructField('duration', IntegerType(), True)
+            ])
 
-s3_client = boto3.client('s3',
-            aws_access_key_id=aws_access_key,
-            aws_secret_access_key=aws_secret_key)
+def grab_s3_contents():
+    """ Connects to s3, grabs an object from bucket, 
+        and returns the contents of object.
+    """
+    s3_client = boto3.client('s3',
+                aws_access_key_id=aws_access_key,
+                aws_secret_access_key=aws_secret_key)
+    file_object = s3_client.get_object(Bucket='la-ticket-bucket-eu', 
+                                            Key='BI_logs/u_ex100106.log')
+    file_contents = file_object['Body'].read().decode().split('\n')
 
-######## CODE TO ITERATE THROUGH ALL OBJECTS IN S3 BUCKET ########
-# bucket_contents = s3_client.list_objects(Bucket='la-ticket-bucket-eu', Prefix='BI_logs_small/')
-# file_names = []
-# for i in bucket_contents['Contents']:
-#     file_names.append(i['Key'])
-# file_names.pop(0) # remove 'BI_logs' folder name from list, because it's not a specific file name
-# for log in file_names:
-######## DOES NOT WORK FOR MULTIPLE FILES, NEED TO INVESTIGATE ########
-
-file_object = s3_client.get_object(Bucket='la-ticket-bucket-eu', 
-                                        Key='BI_logs/u_ex100106.log')
-file_contents = file_object['Body'].read().decode().split('\n')
+    return file_contents
 
 
-# logs with 18 columns
-logs_18 = []
-# logs with 14 columns
-logs_14 = []
+def transform_logs(file_contents):
+    """ Takes a file's contents, transforms its data types,  
+        converts it to Parquet files, and writes to s3. 
+    """
+    # Logs with 18 columns
+    logs_18 = []
+    # Logs with 14 columns
+    logs_14 = []
 
-for i in file_contents:
-    if i.startswith('#'):
+    # Ignore comments and split each log column by space
+    for i in file_contents:
+        if i.startswith('#'):
+            pass
+        else:
+            if len(i.split(' ')) == 18:
+                logs_18.append(i.split(' '))
+            else:
+                logs_14.append(i.split(' '))
+
+    # if there are no logs with 18 columns, pass
+    if logs_18 == [['']]:
         pass
     else:
-        if len(i.split(' ')) == 18:
-            logs_18.append(i.split(' '))
-        else:
-            logs_14.append(i.split(' '))
+        # Parallelize the logs into an RDD with n partitions
+        para_logs_18 = sc.parallelize(logs_18, 10)
+        # Create a dataframe from RDD using the schema previously made
+        df_18 = spark.createDataFrame(para_logs_18, schema_18)
+        # Write dataframe as n number of Parquet files to S3
+        df_18.write.mode('append').save('s3a://la-ticket-bucket-eu/spark-etl5')
+        print('Wrote DF to spark-etl5 18 columns')
 
-if logs_18 == [['']]:
-    pass
-else:
-    # Parallelize the logs into an RDD with n partitions
-    para_logs_18 = sc.parallelize(logs_18, 10)
-    # Create a dataframe from RDD using the schema previously made
-    df_18 = spark.createDataFrame(para_logs_18, schema_18)
+    if logs_14 == [['']]:
+        pass
+    else:
+        para_logs_14 = sc.parallelize(logs_14, 10)
+        df_14 = spark.createDataFrame(para_logs_14, schema_14)
+        df_14.write.mode('append').save('s3a://la-ticket-bucket-eu/spark-etl5')
+        print('Wrote DF to spark-etl5 14 columns')
 
-    # Convert log columns to appropriate datatypes
-    df_18 = df_18.withColumn('date', df_18['date'].cast(DateType()))
-    df_18 = df_18.withColumn('time', df_18['time'].cast(TimestampType()))
-    df_18 = df_18.withColumn('server_port', df_18['server_port'].cast(IntegerType()))
-    df_18 = df_18.withColumn('status', df_18['status'].cast(IntegerType()))
-    df_18 = df_18.withColumn('substatus', df_18['substatus'].cast(IntegerType()))
-    df_18 = df_18.withColumn('win32_status', df_18['win32_status'].cast(IntegerType()))
-    df_18 = df_18.withColumn('bytes_sent', df_18['bytes_sent'].cast(IntegerType()))
-    df_18 = df_18.withColumn('bytes_received', df_18['bytes_received'].cast(IntegerType()))
-    df_18 = df_18.withColumn('duration', df_18['duration'].cast(IntegerType()))
-
-    # Writes dataframe as n number of Parquet files to S3
-    df_18.write.mode('append').save('s3a://la-ticket-bucket-eu/spark-etl5')
-    print('Wrote DF to spark-etl5 18 columns')
-
-if logs_14 == [['']]:
-    pass
-else:
-    para_logs_14 = sc.parallelize(logs_14, 10)
-    df_14 = spark.createDataFrame(para_logs_14, schema_14)
-    
-    df_14 = df_14.withColumn('date', df_14['date'].cast(DateType()))
-    df_14 = df_14.withColumn('time', df_14['time'].cast(TimestampType()))
-    df_14 = df_14.withColumn('server_port', df_14['server_port'].cast(IntegerType()))
-    df_14 = df_14.withColumn('status', df_14['status'].cast(IntegerType()))
-    df_14 = df_14.withColumn('substatus', df_14['substatus'].cast(IntegerType()))
-    df_14 = df_14.withColumn('win32_status', df_14['win32_status'].cast(IntegerType()))
-    df_14 = df_14.withColumn('duration', df_14['duration'].cast(IntegerType()))
-
-    df_14.write.mode('append').save('s3a://la-ticket-bucket-eu/spark-etl5')
-    print('Wrote DF to spark-etl5 14 columns')
-
-######## Attempt to write from spark directly to AWS Redshift
-######## Apparently this is bad practice, so writing to s3 instead
-# df_18.write \
-#   .format("com.databricks.spark.redshift") \
-#   .option("url", "jdbc:redshift://la-tickets.cwnesvytuyhn.eu-west-1.redshift.amazonaws.com:5439/dev?user=awsuser&password=Lovesaws22") \
-#   .option("dbtable", "sparktest") \
-#   .option("tempdir", "s3://la-ticket-bucket-eu/spark-etl-temp") \
-#   .option("aws_iam_role", "arn:aws:iam::900056063831:role/aws-service-role/redshift.amazonaws.com/AWSServiceRoleForRedshift") \
-#   .mode("append") \
-#   .save()
+if __name__ == '__main__':
+    file_contents = grab_s3_contents()
+    transform_logs(file_contents)
